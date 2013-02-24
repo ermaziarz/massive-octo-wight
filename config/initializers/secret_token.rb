8da8b07d3e24e21a577e127074d3955ae9909592
Erik Maziarz <erik.maziarz@gmail.com>
2013-02-23 23:50:57 -0500

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ErikMaziarzRanc::Application.config.secret_token = '77160c043882af4b7c6eeee69b9d54e91798977af6fd5744ce907da720c5645ecfba39e85221ab6a50c7cc01d12ae70f7616d53ba1b231ac09ca39c18aa48114'
